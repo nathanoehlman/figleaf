5523719ea1ef8a932629a81eab58bd4442ccee75
techtangents <167879+techtangents@users.noreply.github.com>
2022-03-14 12:03:21 +1000

    Update packages. Update code style. Use "expect" instead of "should". Check in package-lock.json. Avoid some mutation. Fix readme markup.

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-var _ = require('lodash'),
-  	path = require('path'),
-  	fs = require('fs'),  	
-  	config;
-  
-module.exports = function(opts) {
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs');
 
-	opts = opts || {};
-	if (config && !opts.refresh) return config;
+let config;
+module.exports = opts => {
 
-	var searchPaths = [],
-		environment = process.env.NODE_ENV || 'development';
+  opts = opts || {};
+  if (config && !opts.refresh) return config;
 
-	if (opts.env && process.env[opts.env]) searchPaths.push(process.env[opts.env]);
-	if (opts.paths) searchPaths = searchPaths.concat(opts.paths);
-	searchPaths.push(path.join(process.cwd(), 'config'));
+  let searchPaths = []
+  const environment = process.env.NODE_ENV || 'development';
 
-	var foundPath = _.find(searchPaths, function(configPath) {
-			return fs.existsSync(configPath);
-		});
-	if (!foundPath) throw new Error('No configuration paths found');
-	  
-	var configFiles = [];
-	['default.json', environment + ('.' + (opts.extension || 'json'))].forEach(function(filename) {
-		var filepath = path.join(foundPath, filename);
-		if (fs.existsSync(filepath)) {
-			configFiles.push(path.join(foundPath, filename));
-		}
-	});
-	if (configFiles.length === 0) throw new Error('No configuration files found');
+  if (opts.env && process.env[opts.env]) searchPaths.push(process.env[opts.env]);
+  if (opts.paths) searchPaths = searchPaths.concat(opts.paths);
+  searchPaths.push(path.join(process.cwd(), 'config'));
 
-	// read the local configuration files and override as appropriate
-	config = _.merge.apply(_, [{}].concat(configFiles.map(require)));
-	return config;
+  const foundPath = _.find(searchPaths, configPath => fs.existsSync(configPath));
+  if (!foundPath) throw new Error('No configuration paths found');
+
+  const configFiles = [];
+  ['default.json', environment + ('.' + (opts.extension || 'json'))].forEach(filename => {
+    const filepath = path.join(foundPath, filename);
+    if (fs.existsSync(filepath)) {
+      configFiles.push(path.join(foundPath, filename));
+    }
+  });
+  if (configFiles.length === 0) throw new Error('No configuration files found');
+
+  // read the local configuration files and override as appropriate
+  config = _.merge.apply(_, [{}].concat(configFiles.map(require)));
+  return config;
 }
